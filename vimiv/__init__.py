3d037ed7142ed7b1c7382eded4de6443050543ee
karlch <christian.karl@protonmail.com>
2016-07-10 17:08:44 +0200

    Remove standard imports from check in init

--- vimiv/__init__.py
+++ vimiv/__init__.py
@@ -2,20 +2,9 @@
 # encoding: utf-8
 
 try:
-    import argparse
-    import configparser
-    import mimetypes
-    import os
-    import re
-    import shutil
-    import signal
-    import sys
     from gi import require_version
     require_version('Gtk', '3.0')
     from gi.repository import GLib, Gtk, Gdk, GdkPixbuf, Pango
-    from random import shuffle
-    from subprocess import Popen, PIPE
-    from threading import Thread
     from PIL import Image, ImageEnhance
 
 except ImportError as import_error:
