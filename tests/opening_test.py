9460850535fd3c5e3daa4cf16756fde9a14283d6
karlch <christian.karl@protonmail.com>
2016-07-30 14:55:22 +0200

    Test for image with whitespace and symlink in dir

--- tests/opening_test.py
+++ tests/opening_test.py
@@ -61,13 +61,29 @@ class OpeningTest(TestCase):
         self.assertEqual(expected_images, vimiv.paths)
         os.chdir("..")
 
+    def test_opening_with_whitespace(self):
+        """ Open an image with whitespace and symlink in directory """
+        expected_dir = os.path.abspath("./testimages/directory/")
+        paths, index = \
+                populate(["testimages/directory/symlink with spaces .jpg"],
+                         False, False)
+        vimiv = v_main.Vimiv(self.settings, paths, index)
+        vimiv.main(True)
+        # Check moving and image population
+        self.assertEqual(expected_dir, os.getcwd())
+        expected_images = ["symlink with spaces .jpg"]
+        expected_images = [os.path.abspath(image) for image in expected_images]
+        self.assertEqual(expected_images, vimiv.paths)
+        os.chdir("../..")
+
     def test_opening_recursively(self):
         """ Open all images recursively """
         paths, index = populate([], True, False)
         vimiv = v_main.Vimiv(self.settings, paths, index)
         vimiv.main(True)
-        self.assertEqual(5, len(vimiv.paths))
+        self.assertEqual(6, len(vimiv.paths))
         os.chdir("..")
+
 
 
 if __name__ == '__main__':
