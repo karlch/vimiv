3f154564694e10776052e24ce47782adeaeba4b9
karlch <christian.karl@protonmail.com>
2016-08-05 22:02:12 +0200

    Fix Makefile for AUR

--- icons/install_icons.sh
+++ icons/install_icons.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+DESTDIR=$1
+
 for i in 16 32 64 128 256 512; do
-    cp icons/vimiv_${i}x${i}.png /usr/share/icons/hicolor/${i}x${i}/apps/vimiv.png
+    install -Dm644 icons/vimiv_${i}x${i}.png ${DESTDIR}/usr/share/icons/hicolor/${i}x${i}/apps/vimiv.png
 done
 
-cp icons/vimiv.svg /usr/share/icons/hicolor/scalable/apps/vimiv.svg
+install -Dm644 icons/vimiv.svg ${DESTDIR}/usr/share/icons/hicolor/scalable/apps/vimiv.svg
--- icons/remove_icons.sh
+++ icons/remove_icons.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+DESTDIR=$1
+
 for i in 16 32 64 128 256 512; do
-    rm /usr/share/icons/hicolor/${i}x${i}/apps/vimiv.png
+    rm ${DESTDIR}/usr/share/icons/hicolor/${i}x${i}/apps/vimiv.png
 done
 
-rm /usr/share/icons/hicolor/scalable/apps/vimiv.svg
+rm ${DESTDIR}/usr/share/icons/hicolor/scalable/apps/vimiv.svg
