5beb5ff7645429e225b524570ffd0b83c92c5730
karlch <christian.karl@protonmail.com>
2016-08-16 17:19:36 +0200

    Fix shellcheck

--- tests/create_testimages.sh
+++ tests/create_testimages.sh
@@ -2,14 +2,16 @@
 
 # Simple script to fetch the images used in vimiv's testsuite
 
-cd tests 1>/dev/null 2>&1
+(
+cd tests 1>/dev/null 2>&1 || exit 1
 if [[ -d  "vimiv/testimages" ]]; then
+    (
     printf "updating testimages\n"
-    cd vimiv 1>/dev/null 2>&1
+    cd vimiv 1>/dev/null 2>&1 || exit 1
     git pull
-    cd ..
+    )
 else
     printf "receiving testimages\n"
     git clone --branch=testimages https://github.com/karlch/vimiv
 fi
-cd ..
+)
