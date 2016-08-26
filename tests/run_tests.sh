ab30e0be04c871b3da5cf1fde1b5cb4be65cab72
karlch <christian.karl@protonmail.com>
2016-08-26 12:17:51 +0200

    Improve tests script.
    Only kill the just started Xvfb and herbstluftwm.
    Run the main_test creating all necessary stuff before doing anything else.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -17,15 +17,19 @@ else
 fi
 )
 
-# Start Xvfb if necessary
+# Start Xvfb if possible
 if [[ ! -f /tmp/.X1-lock ]]; then
-    Xvfb -screen 1 800x600x24 :1 &
+    Xvfb -screen 1 800x600x24 :1 & xvfb_pid="$!"
     sleep 2
-    DISPLAY=":1" herbstluftwm &
+    DISPLAY=":1" herbstluftwm & herbst_pid="$!"
     sleep 1
+    # We need this so all directories are generated before running tests.
+    (DISPLAY=":1" python3 tests/main_test.py 1>/dev/null 2>&1 && \
+        printf "Vimiv tests can be run.\n\n") || \
+        (printf "Main test failed.\n" && exit 1)
     DISPLAY=":1" nosetests
-    pkill herbstluftwm
-    pkill Xvfb
+    kill "$xvfb_pid"
+    kill "$herbst_pid"
 else
     printf "Close the running Xserver running on DISPLAY :1 to start Xvfb.\n"
     exit 1
