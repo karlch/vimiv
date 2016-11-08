af78e01d1f5d98fda665df42262f3c2ae89ce7cb
karlch <christian.karl@protonmail.com>
2016-11-08 20:49:51 +0100

    Explicitly run nosetests3 and fix if statement
    On systems with python2 as default nosetests is the python2 version and
    therefore does not work with the vimiv testsuite.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -18,18 +18,18 @@ fi
 )
 
 # Start Xvfb if possible
-if [[ ! -f /tmp/.X1-lock ]]; then
+if [[ ! -f /tmp/.X42-lock ]]; then
     Xvfb :42 -screen 0 800x600x24 1>/dev/null 2>&1 & xvfb_pid="$!"
     sleep 2
 
     # We need this so all directories are generated before running tests.
     (DISPLAY=":42" python3 tests/main_test.py 1>/dev/null 2>&1 && \
-        printf "Vimiv tests can be run.\n\n" && DISPLAY=":42" nosetests) || \
+        printf "Vimiv tests can be run.\n\n" && DISPLAY=":42" nosetests3) || \
         (printf "Main test failed.\n" && exit 1)
 
     kill "$xvfb_pid"
     printf "Killed Xvfb.\n"
 else
-    printf "Close the running Xserver running on DISPLAY :1 to start Xvfb.\n"
+    printf "Close the running Xserver running on DISPLAY :42 to start Xvfb.\n"
     exit 1
 fi
