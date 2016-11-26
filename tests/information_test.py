79cf771c9ee06be6099d2bebce16e83170bba29d
karlch <christian.karl@protonmail.com>
2016-11-26 14:03:12 +0100

    Add test for new information class

---
+++ tests/information_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""Test the Information class for vimiv's test suite."""
+
+from unittest import TestCase, main
+from vimiv.information import Information
+
+
+class InformationTest(TestCase):
+    """Information Tests.
+
+    The pop-up tests simply run through the pop-ups not checking for anything.
+    If all attributes are set correctly, not much can go wrong.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.information = Information()
+
+    def test_print_version(self):
+        """Print version information to screen."""
+        version = self.information.get_version()
+        self.assertIn("0.7.2-devel", version)
+
+    def test_show_version_info(self):
+        """Show the version info pop-up."""
+        self.information.show_version_info(True)
+
+    def test_show_licence(self):
+        """Show the licence pop-up."""
+        self.information.show_licence(None, True)
+
+
+if __name__ == '__main__':
+    main()
