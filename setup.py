b91171646d37eb440fc258cce0b6628dce6b1d20
karlch <karlch@protonmail.com>
2017-03-31 14:23:13 +0200

    Set version to 0.9.0.dev0
    The changes concerning XDG_*_HOME are sustantial enough to increase the
    minor version instead of only the patch.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="vimiv",
-    version="0.8.1.dev0",
+    version="0.9.0.dev0",
     packages=['vimiv'],
     scripts=['vimiv/vimiv'],
     install_requires=['pillow', 'PyGObject'],
